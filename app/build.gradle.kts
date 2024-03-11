9769d2e8a66430fa8cc854ae1b862da9977caf51
BonginkosiD <bonginkosid@vatit.com>
2024-03-11 02:32:11 +0200

    fix: making sure project build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,11 +24,21 @@ android {
             )
         }
     }
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
     testImplementation ("org.mockito:mockito-core:5.7.0")
-    // Add other dependencies as needed
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("org.robolectric:robolectric:4.5.1")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation ("androidx.test:runner:1.4.0")
+    androidTestImplementation ("androidx.test:rules:1.4.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation ("androidx.appcompat:appcompat:1.3.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        //classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+        classpath("com.android.tools.build:gradle:7.4.1")
+        classpath("com.google.gms:google-services:4.3.15")
+
     }
 }
 
